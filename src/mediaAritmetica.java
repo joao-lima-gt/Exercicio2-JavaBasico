fc3d8d49dc15d53aad25009d735770c64220e375
João Pedro N. Lima <joao.lima@groove.tech>
2021-04-20 12:51:08 -0300

    main

---
+++ src/mediaAritmetica.java
@@ -0,0 +1,13 @@
+
+public class mediaAritmetica {
+	public static void main(String [] args) {
+		int a, b, c;
+		
+		a = (8+9+7)/3;
+		b = (4+5+6)/3;
+		c = (a+b)/2;
+		
+		System.out.println("A média das médias é:" + c);
+	}
+
+}
